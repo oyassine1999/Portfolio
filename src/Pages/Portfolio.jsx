64a1a645385c714325b114c51e8fd77a77333c60
oyassine1911 <thvas1911@gmail.com>
2023-01-30 18:40:57 -0700

    finalised portfolio

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,38 +1,48 @@
-import { React } from "react";
+import React from "react";
 import Gif1 from "./assets/gif1.gif";
 import Gif2 from "./assets/gif2.gif";
 import Gif3 from "./assets/gif3.gif";
 import Gif4 from "./assets/gif4.gif";
+import BackgroundGif from "./assets/background.gif";
 
 export default function Portfolio() {
   const redirectToSite = (url) => {
-    window.open(url, '_blank');
+    window.open(url, "_blank");
   };
 
   return (
-    <div className="gifcards">
-      <img
-        src={Gif1}
-        alt="gif1"
-        onClick={() =>
-          redirectToSite("https://github.com/oyassine1999/E-Commerce")
-        }
-      />
-      <img
-        src={Gif2}
-        alt="gif2"
-        onClick={() => redirectToSite("http://site2.com")}
-      />
-      <img
-        src={Gif3}
-        alt="gif3"
-        onClick={() => redirectToSite("http://site3.com")}
-      />
-      <img
-        src={Gif4}
-        alt="gif4"
-        onClick={() => redirectToSite("http://site4.com")}
-      />
+    <div
+      style={{
+        backgroundImage: `url(${BackgroundGif})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+      }}
+    >
+      <div className="gifcards">
+        <img
+          src={Gif1}
+          alt="gif1"
+          onClick={() =>
+            redirectToSite("https://github.com/oyassine1999/E-Commerce")
+          }
+        />
+        <img
+          src={Gif2}
+          alt="gif2"
+          onClick={() => redirectToSite("http://site2.com")}
+        />
+        <img
+          src={Gif3}
+          alt="gif3"
+          onClick={() => redirectToSite("http://site3.com")}
+        />
+        <img
+          src={Gif4}
+          alt="gif4"
+          onClick={() => redirectToSite("http://site4.com")}
+        />
+      </div>
     </div>
   );
 }
